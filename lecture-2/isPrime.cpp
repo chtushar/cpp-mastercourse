023c1cbda430961596e9d8a28b88a5f8950b16b5
Tushar Choudhari <choudharit23@gmail.com>
2020-06-04 12:55:00 +0530

    doubt cleared

--- lecture-2/isPrime.cpp
+++ lecture-2/isPrime.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
-#include<bits/stdc++.h> 
+#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
     cin >> n;
 
-    for(int i = 2; i < sqrt(n); i++){
-        if(n%i == 0){
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
             cout << n << " is not a prime" << endl;
             return 0;
         }
