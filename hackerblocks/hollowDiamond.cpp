349921967f04bd09408bd98a1b9663eadbe47cc7
Tushar Choudhari <choudharit23@gmail.com>
2020-06-07 00:03:57 +0530

    lecture-4

---
+++ hackerblocks/hollowDiamond.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void printStars(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "*\t";
+    }
+}
+
+void printSpaces(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << " \t";
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        }
+
+    return 0;
+}
