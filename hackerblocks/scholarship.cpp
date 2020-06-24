8a8403dd05bf971ed22f5d3080d8747f16aef5fa
Tushar Choudhari <choudharit23@gmail.com>
2020-06-24 14:07:44 +0530

    cb scholarship

--- hackerblocks/scholarship.cpp
+++ hackerblocks/scholarship.cpp
@@ -4,9 +4,23 @@ using namespace std;
 
 int main()
 {
-    int N, M, X, Y;
+    int N, M, X, Y, count = 0;
     cin >> N >> M >> X >> Y;
 
-    
+    for (int i = 0; i < N; i++)
+    {
+        if (M >= X)
+        {
+            count++;
+            M -= X;
+        }
+        else
+        {
+            M += Y;
+        }
+    }
+
+    cout << count << endl;
+
     return 0;
 }
