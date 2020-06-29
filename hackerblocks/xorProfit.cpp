9513dab5fda0a432258597e5aaa70c357ba73656
Tushar Choudhari <choudharit23@gmail.com>
2020-06-29 13:44:54 +0530

    xor profit

--- hackerblocks/xorProfit.cpp
+++ hackerblocks/xorProfit.cpp
@@ -1,12 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
 
-    int x, y, c;
+    int x, y, max = INT_MIN;
 
-    
+    cin >> x;
+    cin >> y;
+
+    for (int i = x; i <= y; i++)
+    {
+        for (int j = x; j <= y; j++)
+        {
+            int c;
+            c = i ^ j;
+
+            if (c > max)
+            {
+                max = c;
+            }
+        }
+    }
+
+    cout << max << endl;
+
     return 0;
 }
